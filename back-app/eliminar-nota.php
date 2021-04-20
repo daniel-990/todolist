1b6426ad8943722e3fb89919ec2b57195f47b864
daniel-990 <danielarango990@gmail.com>
2021-04-20 18:49:14 -0500

    se agrega eliminar nota

---
+++ back-app/eliminar-nota.php
@@ -0,0 +1,25 @@
+<?php 
+
+    require '../constantes/conectar.php';
+
+    $id = $_GET['id'];
+
+    if($conexion){
+        if(isset($_POST)){
+
+            $sql = "DELETE FROM todolist WHERE id='$id'";
+            if(mysqli_query($conexion, $sql)){
+                header("Location: https://listasdetareas.herokuapp.com/");
+                exit;
+            }else{
+                echo "Error al enviar la solicitud ".$sql. "<br>" .mysqli_error($conexion);
+            }
+        }else{
+            echo "no hay datos!";
+        }
+    }else{
+        echo "no esta conectado!!";
+    }
+
+
+?>
