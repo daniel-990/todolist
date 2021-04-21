2d2e8763010c0481bfbc0cc04ae3b35042b2b9e9
daniel-990 <danielarango990@gmail.com>
2021-04-20 19:05:25 -0500

    se actualiza ingresar nota

--- back-app/_ingresar-nota.php
+++ back-app/_ingresar-nota.php
@@ -7,12 +7,12 @@
 
     if(isset($_POST)){
         if($nombreNota == "" && $nota == ""){
-            header("Location: http://localhost/todolist/index.php?mensajeError=No se genero la nota");
+            header("Location: https://listasdetareas.herokuapp.com/index.php?mensajeError=No se genero la nota");
         }else{
             if($conexion){
                 $sql = "INSERT INTO todolist (nombre_nota, nota) VALUES ('$nombreNota','$nota')";
                 if(mysqli_query($conexion, $sql)){
-                    header("Location: http://localhost/todolist/index.php?mensaje=Nota guardada");
+                    header("Location: https://listasdetareas.herokuapp.com/index.php?mensaje=Nota guardada");
                     exit;
                 }else{
                     echo "error!";
