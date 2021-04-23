96aee29c533343f21d29f5315acd2e83319e633b
daniel-990 <danielarango990@gmail.com>
2021-04-23 13:45:17 -0500

    mensaje

--- partials/menu.php
+++ partials/menu.php
@@ -12,6 +12,6 @@
     <li><a href="/"><i class="material-icons color-negro">data_usage</i> TodoList</a></li>
     <li><a href="login.php"><i class="material-icons dp48">person_pin</i> Login</a></li>
     <li><a href="registro.php"><i class="material-icons dp48">person_add</i> Registro</a></li>
-    <li><a href="/todolist/back-app/login-cerrar.php"><i class="material-icons dp48">security</i> Cerrar sesion</a></li>
+    <li><a href="/back-app/login-cerrar.php"><i class="material-icons dp48">security</i> Cerrar sesion</a></li>
 </ul>
 <a data-target="slide-out" class="z-depth-2 sidenav-trigger btn-floating btn-large waves-effect waves-light red boton_menu"><i class="material-icons dp48">menu</i></a>
