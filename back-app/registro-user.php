7b56d81b9bd237a62fa42448d9a27ee76cc44618
Daniel Arango Villegas <danielarangovillegas@MacBook-Pro-de-Daniel.local>
2022-02-18 20:40:51 -0500

    cambio en el documento de registro

--- back-app/registro-user.php
+++ back-app/registro-user.php
@@ -15,20 +15,35 @@
             exit;
         }else{
             if($conexion){
+
+                // Realizamos la consulta para saber si coincide con uno de esos criterios
                 $sql = "INSERT INTO registrouser (correoregistro, passregistro) VALUES ('$registroNombreUser','$password')";
-                if(mysqli_query($conexion, $sql)){
-                        $sql = "SELECT IdUser FROM registrouser";
-                        $datos = array();
-                        $result = $conexion->query($sql);
-                            if($result->num_rows > 0){
-                                while($row = $result->fetch_assoc()) {
-                                    header("Location: https://listasdetareas.herokuapp.com/login.php?mensaje=Usuario: ".$registroNombreUser." registrado&IdUser=".$row['IdUser']);
-                                    //header("Location: http://localhost/todolist/index.php?mensaje=Usuario: ".$registroNombreUser." registrado&IdUser=".$row['IdUser']);
-                                }
-                            }else{}
+                $result = mysqli_query($conexion, $sqlRevisar);
+                
+                // Validamos si hay resultados
+                if(mysqli_num_rows($result)>0){
+                    // Si es mayor a cero imprimimos que ya existe el usuario
+                    header("Location: ./index.php?mensaje=Ya existe el usuario:<strong> ".$registroNombreUser." </strong>que intenta registrar");
+                    exit;
                 }else{
-                    echo "error!";
+
+                    $sql = "INSERT INTO registrouser (correoregistro, passregistro) VALUES ('$registroNombreUser','$password')";
+                    if(mysqli_query($conexion, $sql)){
+                            $sql = "SELECT IdUser FROM registrouser";
+                            $datos = array();
+                            $result = $conexion->query($sql);
+                                if($result->num_rows > 0){
+                                    while($row = $result->fetch_assoc()) {
+                                        header("Location: https://listasdetareas.herokuapp.com/login.php?mensaje=Usuario: ".$registroNombreUser." registrado&IdUser=".$row['IdUser']);
+                                        //header("Location: http://localhost/todolist/index.php?mensaje=Usuario: ".$registroNombreUser." registrado&IdUser=".$row['IdUser']);
+                                    }
+                                }else{}
+                    }else{
+                        echo "error!";
+                    }
+
                 }
+                // Cerramos la conexión
                 mysqli_close($conexion);
             }
         }
