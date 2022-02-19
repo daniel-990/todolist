47a0dc9bccab5b38fb07003670e81e20caf6de5d
Daniel Arango Villegas <danielarangovillegas@MacBook-Pro-de-Daniel.local>
2022-02-18 20:55:01 -0500

    se modifico el archivo de registro de usuario

--- back-app/registro-user.php
+++ back-app/registro-user.php
@@ -17,7 +17,7 @@
             if($conexion){
 
                 // Realizamos la consulta para saber si coincide con uno de esos criterios
-                $sql = "INSERT INTO registrouser (correoregistro, passregistro) VALUES ('$registroNombreUser','$password')";
+                $sqlRevisar = "INSERT INTO registrouser (correoregistro, passregistro) VALUES ('$registroNombreUser','$password')";
                 $result = mysqli_query($conexion, $sqlRevisar);
                 
                 // Validamos si hay resultados
