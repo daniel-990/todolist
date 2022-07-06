e3dd29d0fd571e74bc1f5638718e5c964f203e64
Daniel Arango <danielarango990@gmail.com>
2022-07-06 09:37:17 -0500

    se edita el archivo de credenciales
    se edita el archivo de credenciales

--- constantes/constantes.php
+++ constantes/constantes.php
@@ -1,22 +1,13 @@
 <?php 
 
     //base de datos
-    define('SERVER','bjoibyiqiyvjwyxzpm84-mysql.services.clever-cloud.com');
-    define('USER','ueamjdjmr2pcpuzi');
-    define('PASS','jGY47l3DLffuKbXoxej9');
-    define('DB','bjoibyiqiyvjwyxzpm84');
+    define('SERVER','');
+    define('USER','');
+    define('PASS','');
+    define('DB','');
 
     //correo
-    define('EMAIL_SENDER', 'danielarangopagina@gmail.com');
-    define('EMAIL_PASS', 'K16bdNhFyQfw');
-
-    $pass = "dam";
-    $passHash = password_hash($pass, PASSWORD_BCRYPT);
-    
-    if(password_verify("dam",$passHash)){
-        //echo "si";
-    }else{
-        //echo "no";
-    }
+    define('EMAIL_SENDER', '');
+    define('EMAIL_PASS', '');
 
 ?>
